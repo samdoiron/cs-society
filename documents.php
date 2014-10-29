080b9ec1b05b012add2919b952412f03bc5bde10
Matthew Coelho <mcoelho92@gmail.com>
2014-10-29 17:56:18 -0300

    added php includes

---
+++ documents.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/head.html'); ?>
+  <body>
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/nav.html'); ?>
+
+    <!-- content-->
+    <div class="container">
+      
+      <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/footer.html'); ?>
+    </div> <!-- /container -->
+    
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/includes/js.html'); //this should be last ?>
+  </body>
+</html>
