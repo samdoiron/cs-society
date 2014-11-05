3e33401cb89dfa38285ae1f84c030cd78acdd958
Sam Doiron <samdoiron@fastmail.fm>
2014-11-05 19:45:48 -0400

    Add cs society footer

--- index.php
+++ index.php
@@ -17,20 +17,22 @@
 			</div>
     </div>
 
-    <div class="ui segment">
-      <div class="ui stackable grid">
-        <div class="ui eight wide column">
-          <h2>Geekbeer</h2>
-          <p>We do have them (sometimes)! You could put a blurb about the events or some other stuff here. users can click learn more to do the self explanatory task...</p>
-          <p><a class="ui button" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="ui eight wide column">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="ui button" href="#" role="button">View details &raquo;</a></p>
-        </div>
+		<div class="ui segment">
+			<div class="ui stackable grid">
+				<div class="ui eight wide column">
+					<h2>Geekbeer</h2>
+					<p>We do have them (sometimes)! You could put a blurb about the events or some other stuff here. users can click learn more to do the self explanatory task...</p>
+					<p><a class="ui button" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="ui eight wide column">
+					<h2>Heading</h2>
+					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					<p><a class="ui button" href="#" role="button">View details &raquo;</a></p>
+				</div>
+			</div>
+		</div>
+
       <?php require('./includes/footer.html'); ?>
-    </div> <!-- /container -->
     <?php require('./includes/js.html'); // this should be last ?>
   </body>
 </html>
