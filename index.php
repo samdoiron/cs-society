fec22337ccd394c4e18912a909c23fa9a07d4e12
Sam Doiron <samdoiron@fastmail.fm>
2014-11-05 20:03:05 -0400

    SMall tweaks

--- index.php
+++ index.php
@@ -22,12 +22,12 @@
 				<div class="ui eight wide column">
 					<h2>Geekbeer</h2>
 					<p>We do have them (sometimes)! You could put a blurb about the events or some other stuff here. users can click learn more to do the self explanatory task...</p>
-					<p><a class="ui button" href="#" role="button">View details &raquo;</a></p>
+					<p><a class="ui basic button" href="#" role="button">View details &raquo;</a></p>
 				</div>
 				<div class="ui eight wide column">
 					<h2>Heading</h2>
 					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-					<p><a class="ui button" href="#" role="button">View details &raquo;</a></p>
+					<p><a class="ui basic button" href="#" role="button">View details &raquo;</a></p>
 				</div>
 			</div>
 		</div>
